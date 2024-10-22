d2c172c368faf7dafbc93689680d4c8cc58b14cc
Autania <amierows@ucsc.edu>
2024-10-21 17:27:59 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,23 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Autania Mierow
+// Date: October 21st, 2024
 
-// Constants
+// Define Variables
+make="Honda";
+model= "Civic";
+color= "Black";
+year= "2012";
 
-// Functions
+// Calculate 
+age= 2024 - year; 
+
+//output
+document.writeln("Make"+ make + "<br>");
+document.writeln("Model"+ model + "<br>");
+document.writeln("Color"+ color + "<br>");
+document.writeln("Year"+ year + "<br>");
+document.writeln("Age"+ age + "<br>");
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
