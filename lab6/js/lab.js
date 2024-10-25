9a1446e8566819eeab3da6071822ac894e0adfca
Autania <amierows@ucsc.edu>
2024-10-24 19:44:12 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,34 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Autania Mierow
+// Date: 10 / 24/ 2024
 
 // Constants
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+function myFunction() {
+  // Variables
+  myTransport = ["honda", "bus", "rides from friends"];
+
+  // Object for main ride
+  myMainRide = {
+    make: "honda",
+    model: "civic",
+    color: "black",
+    year: 2012,
+    age: function () {
+      return new Date().getFullYear() - this.year;
+    }
+  };
 }
+
+// Output
+document.writeln("Kinds of Transportation I use: ", myTransport.join(", "), "</br>");
+document.writeln("My Main Ride:<pre>" + JSON.stringify(myMainRide, null, '\t') + "</pre>");
 
 function main() {
   console.log("Main function started.");
-  // the code that makes everything happen
 }
 
-// let's get this party started
 main();
