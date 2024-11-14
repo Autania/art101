41035d001350651aeae66a006dfa50be9730a899
Autania <amierows@ucsc.edu>
2024-11-14 12:13:58 -0800

    lab12

---
+++ lab12/js/lab.js
@@ -0,0 +1,29 @@
+
+// Author: Autania Mierow
+// Date: 14 November 2024
+
+function sortingHat(str) {
+  let len = str.length;
+  let mod = len % 4;
+
+  if (mod === 0) {
+    return "Gryfindor";
+
+  } else if (mod === 1) {
+    return "Slytherin";
+
+  } else if (mod === 2) {
+    return "Hufflepuff";
+
+  } else if (mod === 3) {
+    return "Ravenclaw :(";
+  }
+}
+
+var myButton = document.getElementById("button");
+myButton.addEventListener("click", function() {
+  var name = document.getElementById("input").value;
+  var house = sortingHat(name);
+  var newText = "<p>Here's your answer: " + house + "</p>";
+  document.getElementById("output").innerHTML = newText;
+});
