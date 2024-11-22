ceb797ff8da2976bc2785b7371d21d267e8d5213
Autania <amierows@ucsc.edu>
2024-11-21 21:40:25 -0800

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,18 +5,22 @@
     Date: NOV 21
 */
 
-// Append buttons to each section
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
-$("#problems").append("<button id='button-problems'>Make Special</button>");
-$("#results").append("<button id='button-results'>Make Special</button>");
+$(document).ready(function() {
+    console.log("Document is ready!");
 
-// Add click listeners to toggle the "special" class
-$("#button-challenge").click(function() {
-    $("#challenge").toggleClass("special");
-});
-$("#button-problems").click(function() {
-    $("#problems").toggleClass("special");
-});
-$("#button-results").click(function() {
-    $("#results").toggleClass("special");
+    // Add buttons to sections
+    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
+    $("#problems").append("<button id='button-problems'>Make Special</button>");
+    $("#results").append("<button id='button-results'>Make Special</button>");
+
+    // Add click listeners for toggling the "special" class
+    $("#button-challenge").click(function() {
+        $("#challenge").toggleClass("special");
+    });
+    $("#button-problems").click(function() {
+        $("#problems").toggleClass("special");
+    });
+    $("#button-results").click(function() {
+        $("#results").toggleClass("special");
+    });
 });
